9d38e6ed864c777a7ec464e85a16862a74d7b938
Bugulto <pl03197222@student.ku.edu.np>
2026-06-16 18:22:00 +0545

    Cleaning work directory

--- services/inference/service.py
+++
@@ -1,8 +0,0 @@
-class inferenceService:
-    def __init__(self, model_adapter, preprocessor):
-        self.model_adapter = model_adapter
-        self.preprocessor = preprocessor
-
-    def predict(self, frame):
-        processed_frame = self.preprocessor.process(frame)
-        return self.model_adapter.predict(processed_frame)
